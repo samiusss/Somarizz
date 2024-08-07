f6048c8a649a3d84faf4d6d1626556029e02dd45
Sami Sabir <sasab21@ulaval.ca>
2024-08-07 16:07:46 -0400

    All basic features added and functional

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,10 +4,19 @@ import {copy, linkIcon, tick, loader} from '../assets';
 import {useLazyGetSummaryQuery} from "../services/article";
 //show the summary part of the article
 const Demo = () => {
+
     const [article, setArticle] = useState({
         url: "",
         summary: "",
     });
+
+    const [copied, setCopied] = useState("")
+
+    const handleCopy = (copyUrl) => {
+        setCopied(copyUrl)
+        navigator.clipboard.writeText(copyUrl);
+        setTimeout(()=> {setCopied(false)}, 3000);
+    }
 
     const [allArticles, setAllArticles] = useState([]);
 
@@ -51,7 +60,6 @@ const Demo = () => {
                         alt="link icon"
                         className="absolute left-0 my-2.5 ml-3 w-5"
                     />
-
                     <input
                         type="url"
                         placeholder="Enter a URL"
@@ -77,9 +85,9 @@ const Demo = () => {
                         onClick={() => setArticle(item)}
                         className="link_card"
                         >
-                            <div className="copy_btn">
+                            <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                                 <img
-                                    src={copy}
+                                    src={copied === item.url ? tick : copy}
                                     alt="copy icon"
                                     className="w-[40%] h-[40%] object-contain"
                                 />
@@ -91,9 +99,32 @@ const Demo = () => {
                     ))}
                 </div>
 
-                {/*Browse URL history */}
-                {/*Display results */}
-            </div>
+                <span className="my-10 max-w-full flex juftify-center items-center">
+                    {isFetching ? (
+                        <img
+                            src={loader}
+                            alt="loading"
+                            className="w-20 h-20 object-contain justify-center items-center"
+                        />
+                    ): error ? (
+                        <p className="font-inter font-bold text-center text-black">
+                            Sorry, no results were found for this URL...
+                            <span className="font-satoshi font-normal text-gray-700">{error?.data?.error}</span>
+                        </p>
+                    ) : (
+                        article.summary && (
+                            <div className="flex flex-col gap-3">
+                                <h2 className="blue_gradient">
+                                    <span className="blue_gradient">Summary</span>
+                                </h2>
+                                <div className="summary_box">
+                                    <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+                                </div>
+                            </div>
+                        )
+                    )}
+                </span>
+                </div>
         </section>
     )
 }
